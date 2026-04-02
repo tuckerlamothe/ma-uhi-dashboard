803fb5b4175384025c64bf38d71a261f50d34921
tuckerlamothe <tucker.lamothe@gmail.com>
2026-04-01 21:40:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -283,8 +283,7 @@ map_output = st_folium(
     height=500, 
     width=None,
     use_container_width=True,
-    returned_objects=["last_active_drawing"],
-    draw_export=True
+    returned_objects=["last_active_drawing"]
 )
 
 st.markdown("""
