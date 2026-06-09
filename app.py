ee641d1dddb040effb65062d635ac9c4bbb213a2
tuckerlamothe <tucker.lamothe@gmail.com>
2026-06-09 12:48:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -439,6 +439,7 @@ if active_geom:
 
             with col_b:
                 st.subheader("Simulation Results")
+                help="See left side menu for simulation options."
                 st.markdown(
                     '<p style="font-size: 14px; color: rgba(192,192,192,0.6); margin-top: -10px; margin-bottom: 20px;">'
                     'What could things be like?'
