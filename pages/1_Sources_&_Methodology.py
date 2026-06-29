01ddbaa797baddaff4dedbcaa58011b3c1d2a625
tuckerlamothe <tucker.lamothe@gmail.com>
2026-06-29 13:16:32 -0400

    Update 1_Sources_&_Methodology.py

--- pages/1_Sources_&_Methodology.py
+++ pages/1_Sources_&_Methodology.py
@@ -16,7 +16,7 @@ st.divider()
 # Massive text sections with ample space
 st.markdown("### 1. Ambient Air Impact Coefficients (Per unit change)")
 st.markdown("###### Air Impact = ((Impervious * $A_{IMP}$) - (Tree Canopy * $A_{TREE}$) - ([Albedo] - 0.15) * $A_{ALB}$)")
-st.markdown("*With a 35°C baseline of 0% impervious, 0% tree canopy, 0.15 albedo*")
+st.markdown("*With a 35°C baseline of 0% impervious, 0% tree canopy, 0.15 albedo. All temperatures are in °C.*")
 st.markdown("""
     **Impervious surface | $A_{IMP}$ = 0.07/%** For each percent more concrete/asphalt in a given area, the simulation adds 0.07°C.  
     **Tree canopy | $A_{TREE}$ = 0.01/%** Tree canopy functions as the offset to higher temperatures, so the above formula **subtracts** tree impact from  total temp.  
