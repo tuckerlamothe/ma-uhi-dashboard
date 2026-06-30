e370f795902ccabe87acb1d843a407ea11a149f7
tuckerlamothe <tucker.lamothe@gmail.com>
2026-06-29 23:59:45 -0400

    Update 1_Sources_&_Methodology.py

--- pages/1_Sources_&_Methodology.py
+++ pages/1_Sources_&_Methodology.py
@@ -18,10 +18,10 @@ st.markdown("### 1. Ambient Air Impact Coefficients (Per unit change)")
 st.markdown("###### Air Impact = (35 + (Impervious * $A_{IMP}$) - (Tree Canopy * $A_{TREE}$) - ([Albedo] - 0.15) * $A_{ALB}$)")
 st.markdown("*With a 35°C baseline of 0% impervious, 0% tree canopy, 0.15 albedo. All temperatures are in °C.*")
 st.markdown("""
-    **Impervious surface | $A_{IMP}$ = 0.0075/%** For each percent additional concrete/asphalt in a given area, the simulation adds 0.0075°C.  
-    **Tree canopy | $A_{TREE}$ = 0.01/%** Tree canopy functions as the offset to higher temperatures, so the above formula **subtracts** tree impact from total temp.  
-    **Green roofs | $A_{GROOF}$ = 0.003/%** Green roofs introduce cooling on the rooftop level. While they are powerful with evapotranspiration and more reflective leaves, their soil traps and releases heat into the night.  
-    **Albedo | $A_{ALB}$ = 6.1** Albedo is measured on a scale of 0 to 1, or 0% to 100% of light absorbed. This coefficient highlights the 6.1°C range across the albedo scale.  
+    **Impervious surface | $A_{IMP}$ = 0.0075/%**  For each percent additional concrete/asphalt in a given area, the simulation adds 0.0075°C.  
+    **Tree canopy | $A_{TREE}$ = 0.01/%**  Tree canopy functions as the offset to higher temperatures, so the above formula **subtracts** tree impact from total temp.  
+    **Green roofs | $A_{GROOF}$ = 0.003/%**  Green roofs introduce cooling on the rooftop level. While they are powerful with evapotranspiration and more reflective leaves, their soil traps and releases heat into the night.  
+    **Albedo | $A_{ALB}$ = 6.1**  Albedo is measured on a scale of 0 to 1, or 0% to 100% of light absorbed. This coefficient highlights the 6.1°C range across the albedo scale.  
     """)
 
 st.markdown("""### 2. Human "Real Feel" Formula (Mean Radiant Temperature)""")
