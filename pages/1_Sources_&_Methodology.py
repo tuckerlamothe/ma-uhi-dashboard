31c0407c5639121c451336a14e9023229fd54b26
tuckerlamothe <tucker.lamothe@gmail.com>
2026-07-05 10:47:34 -0400

    Update 1_Sources_&_Methodology.py

--- pages/1_Sources_&_Methodology.py
+++ pages/1_Sources_&_Methodology.py
@@ -40,7 +40,9 @@ st.markdown("""**Albedo | $A_{ALB}$ = 6.1**
     """)
 st.markdown("""### 2. Human "Real Feel" Formula (Mean Radiant Temperature)""")
 
-st.markdown("""**Impervious surface | $MRT_{IMP}$ = 0.12**""")
+st.markdown("""**Impervious surface | $MRT_{IMP}$ = 0.12**
+    Since concrete both reflects the sun's shortwaves back at your body and also radiates longwaves as it absorbs and releases heat, removing impervious surface can have high cooling potential."
+    """)
 st.markdown(
     "Source: "
     "[Resilient Cambridge Urban Heat Island Report](https://www.cambridgema.gov/-/media/files/cdd/climate/resilientcambridge/urbanheatislandtechnicalreport.pdf?)"
